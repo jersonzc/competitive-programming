2526e9df2eb64bd9cb0f3e180f045a7e713de7b3
Jerson Zuniga <jzunigacoayla@gmail.com>
2025-06-13 18:56:09 -0500

    feat: add optimal solution
    O(m+n)

--- leetcode/88.go
+++ leetcode/88.go
@@ -1,36 +1,32 @@
 package leetcode
 
 func merge(nums1 []int, m int, nums2 []int, n int) {
-	// ni tracks current value in nums2
-	ni := 0
-	v2 := 0
-	if ni < n {
-		v2 = nums2[ni]
-	} else {
-		return
-	}
+	mi := m - 1
+	ni := n - 1
+	ki := m + n - 1
 
-	// iterate over the nums1 array and allocate nums2 current values.
-	it := 0
-	for it < m+n {
-		if v2 < nums1[it] {
-			for j := m + n - 1; j > it; j-- {
-				nums1[j] = nums1[j-1]
-			}
-			nums1[it] = v2
-			ni++
-			if ni < n {
-				v2 = nums2[ni]
-			} else {
-				break
-			}
+	for ki >= 0 {
+		if mi < 0 {
+			nums1[ki] = nums2[ni]
+			ni--
+			ki--
+			continue
 		}
-		it++
-	}
-
-	// allocate nums2 array values that were greater than any value found in nums1.
-	for ni < n {
-		nums1[m+ni] = nums2[ni]
-		ni++
+		if ni < 0 {
+			nums1[ki] = nums1[mi]
+			mi--
+			ki--
+			continue
+		}
+		v1 := nums1[mi]
+		v2 := nums2[ni]
+		if v1 > v2 {
+			nums1[ki] = v1
+			mi--
+		} else {
+			nums1[ki] = v2
+			ni--
+		}
+		ki--
 	}
 }
