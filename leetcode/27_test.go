3e12d98990551e13a335bb3185cb10d794375e40
Jerson Zuniga <jzunigacoayla@gmail.com>
2025-06-15 11:46:25 -0500

    test: replace len and use expected array

--- leetcode/27_test.go
+++ leetcode/27_test.go
@@ -7,75 +7,69 @@ import (
 
 func TestRemoveElement(t *testing.T) {
 	tests := []struct {
-		name         string
-		nums         []int
-		val          int
-		want         int
-		wantElements []int
+		name string
+		nums []int
+		val  int
+		want []int
 	}{
 		{
-			name:         "Test 1",
-			nums:         []int{3, 2, 2, 3},
-			val:          3,
-			want:         2,
-			wantElements: []int{2, 2},
+			name: "Test 1",
+			nums: []int{3, 2, 2, 3},
+			val:  3,
+			want: []int{2, 2},
 		},
 		{
-			name:         "Test 2",
-			nums:         []int{0, 1, 2, 2, 3, 0, 4, 2},
-			val:          2,
-			want:         5,
-			wantElements: []int{0, 1, 3, 0, 4},
+			name: "Test 2",
+			nums: []int{0, 1, 2, 2, 3, 0, 4, 2},
+			val:  2,
+			want: []int{0, 1, 3, 0, 4},
 		},
 		{
-			name:         "Test 3",
-			nums:         []int{2},
-			val:          3,
-			want:         1,
-			wantElements: []int{2},
+			name: "Test 3",
+			nums: []int{2},
+			val:  3,
+			want: []int{2},
 		},
 		{
-			name:         "Test 4",
-			nums:         []int{1, 2},
-			val:          3,
-			want:         2,
-			wantElements: []int{1, 2},
+			name: "Test 4",
+			nums: []int{1, 2},
+			val:  3,
+			want: []int{1, 2},
 		},
 		{
-			name:         "Test 5",
-			nums:         []int{1},
-			val:          1,
-			want:         0,
-			wantElements: []int{},
+			name: "Test 5",
+			nums: []int{1},
+			val:  1,
+			want: []int{},
 		},
 		{
-			name:         "Test 6",
-			nums:         []int{3, 3},
-			val:          3,
-			want:         0,
-			wantElements: []int{},
+			name: "Test 6",
+			nums: []int{3, 3},
+			val:  3,
+			want: []int{},
 		},
 		{
-			name:         "Test 7",
-			nums:         []int{4, 5},
-			val:          5,
-			want:         1,
-			wantElements: []int{4},
+			name: "Test 7",
+			nums: []int{4, 5},
+			val:  5,
+			want: []int{4},
 		},
 		{
-			name:         "Test 8",
-			nums:         []int{4, 5},
-			val:          4,
-			want:         1,
-			wantElements: []int{5},
+			name: "Test 8",
+			nums: []int{4, 5},
+			val:  4,
+			want: []int{5},
 		},
 	}
 
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
 			got := removeElement(tt.nums, tt.val)
-			assertInts(t, got, tt.want)
-			assertKElements(t, tt.want, tt.nums, tt.wantElements)
+			assertInts(t, got, len(tt.want))
+
+			sort.Ints(tt.nums[:got])
+			sort.Ints(tt.want)
+			assertKElements(t, got, tt.nums, tt.want)
 		})
 	}
 }
